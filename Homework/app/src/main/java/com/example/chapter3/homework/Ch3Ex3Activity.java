c877b3638f49d5cdcf71960509e1d4ba4506713d
oufenghua <oufenghua@bytedance.com>
2019-07-27 19:00:20 +0800

    完成作业，R-区枫华

--- Homework/app/src/main/java/com/example/chapter3/homework/Ch3Ex3Activity.java
+++ Homework/app/src/main/java/com/example/chapter3/homework/Ch3Ex3Activity.java
@@ -1,7 +1,17 @@
 package com.example.chapter3.homework;
 
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Pair;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 使用 ViewPager 和 Fragment 做一个简单版的好友列表界面
@@ -9,19 +19,45 @@ import android.os.Bundle;
  * 2. 使用 TabLayout 添加 Tab 支持
  * 3. 对于好友列表 Fragment，使用 Lottie 实现 Loading 效果，在 5s 后展示实际的列表，要求这里的动效是淡入淡出
  */
-public class Ch3Ex3Activity extends AppCompatActivity {
+public class Ch3Ex3Activity extends FragmentActivity {
+
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+
+    private List<Pair<String,Fragment>> fragmentList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ch3ex3);
 
+        // ex3-1. 添加 ViewPager 和 Fragment 做可滑动界面
+        viewPager = findViewById(R.id.viewpager);
+        tabLayout = findViewById(R.id.tab_layout);
 
+        fragmentList.add(new Pair<String, Fragment>("好友列表",PlaceholderFragment.getInstance(true)));
+        fragmentList.add(new Pair<String, Fragment>("我的好友",PlaceholderFragment.getInstance(false)));
 
-        // TODO: ex3-1. 添加 ViewPager 和 Fragment 做可滑动界面
+        viewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int i) {
+                return fragmentList.get(i).second;
+            }
 
+            @Override
+            public int getCount() {
+                return fragmentList.size();
+            }
 
+            @Nullable
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return fragmentList.get(position).first;
+            }
+        });
 
-        // TODO: ex3-2, 添加 TabLayout 支持 Tab
+        // ex3-2, 添加 TabLayout 支持 Tab
+
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
--- Homework/app/src/main/java/com/example/chapter3/homework/PlaceholderFragment.java
+++ Homework/app/src/main/java/com/example/chapter3/homework/PlaceholderFragment.java
@@ -1,33 +1,121 @@
 package com.example.chapter3.homework;
 
 
+import android.animation.ObjectAnimator;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.airbnb.lottie.LottieDrawable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PlaceholderFragment extends Fragment {
 
+    public static String OPTIONS_SHOW_LOADING = "show_loading_option";
+
+    public static int ITEM_COUNT = 30;
+
+    public static final PlaceholderFragment getInstance(boolean showLoading) {
+        PlaceholderFragment fragment = new PlaceholderFragment();
+        Bundle bundle = new Bundle();
+        bundle.putBoolean(OPTIONS_SHOW_LOADING, showLoading);
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+    private boolean showLoading = false;
+
+    private RecyclerView recyclerView;
+    private MyAdapter adapter;
+    private LottieAnimationView animationView;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        showLoading = getArguments().getBoolean(OPTIONS_SHOW_LOADING);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // TODO ex3-3: 修改 fragment_placeholder，添加 loading 控件和列表视图控件
-        return inflater.inflate(R.layout.fragment_placeholder, container, false);
+        // ex3-3: 修改 fragment_placeholder，添加 loading 控件和列表视图控件
+        View view = inflater.inflate(R.layout.fragment_placeholder, container, false);
+        recyclerView = view.findViewById(R.id.recycler_view);
+        animationView = view.findViewById(R.id.animation_view);
+        recyclerView.setAdapter(new MyAdapter());
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+        recyclerView.setVisibility(showLoading ? View.GONE : View.VISIBLE);
+        animationView.setVisibility(showLoading ? View.VISIBLE : View.GONE);
+        if (showLoading) {
+            animationView.setRepeatCount(LottieDrawable.INFINITE);
+            animationView.setRepeatMode(LottieDrawable.REVERSE);
+            animationView.playAnimation();
+        }
+        return view;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        getView().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                // 这里会在 5s 后执行
-                // TODO ex3-4：实现动画，将 lottie 控件淡出，列表数据淡入
-            }
-        }, 5000);
+        if (showLoading) {
+            getView().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    // 这里会在 5s 后执行
+                    // ex3-4：实现动画，将 lottie 控件淡出，列表数据淡入
+                    ObjectAnimator animator = ObjectAnimator.ofFloat(animationView, "alpha", 1f, 0f);
+                    animator.setDuration(1000);
+                    animator.start();
+                    recyclerView.setVisibility(View.VISIBLE);
+                }
+            }, 5000);
+        }
+    }
+
+    class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
+
+        @NonNull
+        @Override
+        public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+            return new MyViewHolder(viewGroup);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int i) {
+            myViewHolder.bind(i);
+        }
+
+        @Override
+        public int getItemCount() {
+            return ITEM_COUNT;
+        }
+    }
+
+    private class MyViewHolder extends RecyclerView.ViewHolder {
+        public MyViewHolder(ViewGroup parent) {
+            super(new TextView(parent.getContext()));
+            itemView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 200));
+        }
+
+        public void bind(int position) {
+            TextView view = ((TextView) itemView);
+            view.setTextSize(24);
+            view.setTextColor(Color.parseColor("#0288D1"));
+            view.setGravity(Gravity.CENTER);
+            view.setText("Item" + (position + 1));
+        }
     }
 }
